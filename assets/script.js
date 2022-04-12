c775ee7730f8ef0891e7dee86d852fd74decbb15
Alfredo Figueroa <alfredojfigueroa@gmail.com>
2022-04-12 18:43:49 -0400

    rewrote the getWeather function for better functionality and to make the code cleaner.

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,20 @@
 // variables for page function //
 var timeDisplayEl = $("#time-display");
-var searchFormEl = $('#search-form');
-var APIkey = '9d478b2327e8d90b0bdf7b581a599de0';
-var cityInputEl = $("#search-input");
-var searchBtn = $('#search-btn')
-var previousSearchEl = $('.recent-searches')
-var currentCity;
+var searchFormCityInputEl = document.querySelector('#search-input');
+var weatherDayCityEl = document.querySelector('#weather-day-city');
+var weatherDayTempEl = document.querySelector('#weather-day-temp');
+var weatherDayWindEl = document.querySelector('#weather-day-wind');
+var weatherDayHumidityEl = document.querySelector('#weather-day-humidity');
+var weatherDayUvIndexEl = document.querySelector('#weather-day-uv-index');
+var forecastContainerEl = document.querySelector('#forecast-conatainer');
+var weatherDayIconEl = document.querySelector('#weather-day-icon');
+var buttonContainerEl = document.querySelector('#button-container');
+var weatherDayContainerEl = document.querySelector('#current-weather-container');
+var outerForecastContainerEl = document.querySelector('#outer-forecast-container');
+var weatherDayDateEl = document.querySelector('#weather-day-date');
+
+var baseUrl = 'http://api.openweathermap.org/';
+var apiKey = '9d478b2327e8d90b0bdf7b581a599de0';
 
 // time display function
 function displayTime() {
@@ -15,115 +24,163 @@ function displayTime() {
 
 setInterval(displayTime, 1000);
 
-function getWeather(data) {
-    var requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={APIkey}`;
+function getCityWeather(city) {
+    var url = `${baseUrl}geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
 
-    fetch(requestUrl)
-        .then(function(response) {
+    fetch(url)
+        .then(function (response) {
             return response.json();
         })
-        .then(function(data) {
+        .then(function (data) {
+            if (!data.length) {
+                window.alert("No city matches.");
+                return;
+            }
+
+            storeCityLocation(city);
+            populateButtond();
+
+            var cityObject = data[0];
+            var lat = cityObject.lat;
+            var lon = cityObject.lon;
+
+            var currentWeatherUrl = `${baseUrl}data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
             // get current weather
-            var currentConditionsEl = document.querySelector('.current-weather-container');
-            currentConditionsEl.classList.addClass('border border-primary');
+            fetch(currentWeatherUrl)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data);
+                    var date = moment(Date.now()).format("L");
+                    var current = data.current;
+                    var temp = current.temp;
+                    var windSpeed = current.wind_speed;
+                    var humidity = current.humidity;
+                    var uvIndex = current.uvi;
+                    var icon = current.weather[0].icon;
 
-            var cityNameEl = $('<h3>');
-            cityNameEl.text(currentCity);
-            currentConditionsEl.append(cityNameEl);
+                    weatherDayCityEl.textContent = city;
+                    weatherDayDateEl.textContent = date;
+                    weatherDayTempEl.textContent = temp;
+                    weatherDayWindEl.textContent = windSpeed;
+                    weatherDayHumidityEl.textContent = humidity;
+                    weatherDayUvIndexEl.textContent = uvIndex;
+                    if (uvIndex < 3) {
+                        weatherDayUvIndexEl.classList.add('favorable');
+                    } else if (uvIndex < 7) {
+                        weatherDayUvIndexEl.classList.add('moderate');
+                    } else {
+                        weatherDayUvIndexEl.classList.add('severe');
+                    }
 
-            // get date for current city
-            var currentCityDate = data.current.dt;
-            currentCityDate = moment.unix(currentCityDate).format('MM/DD/YYYY');
-            var currentDateEl = $('<span>');
-            currentDateEl.text(` (${currentCityDate}) `);
-            cityNameEl.append(currentDateEl);
-
-            var currentCityWeatherIcon = data.current.weather[0].icon;
-            var currentWeatherIconEl = $('<img>');
-            currentWeatherIconEl.attr("src", "http://openweathermap.org/img/wn/" + currentCityWeatherIcon + ".png");
-            cityNameEl.append(currentWeatherIconEl);
-
-            // get temperature data
-            var currentCityTemp = data.current.temp;
-            var currentTempEl = $('<p>')
-            currentTempEl.text(`Temp: ${currentCityTemp}`)
-            currentConditionsEl.append(currentTempEl);
-
-            // get wind
-            var currentCityWind = data.current.wind_speed;
-            var currentWindEl = $('<p>')
-            currentWindEl.text(`Wind: ${currentCityWind}`)
-            currentConditionsEl.append(currentWindEl);
-
-            // get humidity
-            var currentCityHumidity = data.current.humidity;
-            var currentHumidityEl = $('<p>')
-            currentHumidityEl.text(`Humidity: ${currentCityHumidity}`)
-            currentConditionsEl.append(currentHumidityEl);
-
-            // get UV index and set background color based on index
-            var currentCityUv = data.current.uvi;
-            var currentUvEl = $('<p>');
-            var currentUvSpanEl = $('<span>');
-            currentUvEl.append(currentUvSpanEl);
-
-            currentUvSpanEl.text(`UV Index: ${currentCityUv}`)
-
-            if (currentCityUv < 3 ) {
-                currentUvSpanEl.css({'background-color':'green', 'color':'white'});
-            } else if (currentCityUv < 6 ) {
-                currentUvSpanEl.css({'background-color':'yellow', 'color':'black'});
-            } else if (currentCityUv < 8 ) {
-                currentUvSpanEl.css({'background-color':'orange', 'color':'white'});
-            } else if (currentCityUv < 11 ) {
-                currentUvSpanEl.css({'background-color':'red', 'color':'white'});
-            } else {
-                currentUvSpanEl.css({'Background-color':'violet', 'color':'white'});
-            }
-
-            currentConditionsEl.append(currentUvEl);
-            currentConditionsEl.classList.remove("hide");
-
-            // get 5 day forecast
-            var fiveDayForecastEl = $('.forecast-container');
-
-            for (var i = 1; i <=5; i++) {
-                var date;
-                var temp;
-                var icon;
-                var wind;
-                var humidity;
-                var uvIndex;
-
-                date = data.daily[i].dt;
-                date = moment.unix(date).format('MM/DD/YYYY');
-
-                temp = data.daily[i].temp.day;
-                icon = data.daily[i].weather[0].icon;
-                wind = data.daily[i].wind_speed;
-                humidity = data.daily[i].humidity;
-                uvIndex = data.daily[i].uvi;
-
-                // creating cards for each day of forecast
-                var card = document.createElement('section');
-                card.classList.add('card', 'col-2', 'm-1', 'bg-primary', 'text-white');
-
-                var cardBody = document.createElement('section');
-                cardBody.classList.add('card-body')
-                cardBody.innerHTML = `<h4>${date}</h4>
-                    <img src= "https://openweathermap.org/img/wn/${icon}.png"></><br>
-                    ${temp}<br>
-                    ${wind}<br>
-                    ${humidity}<br>
-                    ${uvi}`
-
-                card.appendChild(cardBody)
-                fiveDayForecastEl.append(card);
-                fiveDayForecastEl.classList.remove('hide');
-            }
-        })
-    return;
+                    weatherDayIconEl.src = `https://openweathermap.org/imp/wn/${icon}.png`;
+                    weatherDayContainerEl.classList.remove('hide');
+                    populate5day(data.daily);
+                });
+        });
 }
+    //         var currentConditionsEl = document.querySelector('.current-weather-container');
+    //         currentConditionsEl.classList.addClass('border border-primary');
+
+    //         var cityNameEl = $('<h3>');
+    //         cityNameEl.text(currentCity);
+    //         currentConditionsEl.append(cityNameEl);
+
+    //         // get date for current city
+    //         var currentCityDate = data.current.dt;
+    //         currentCityDate = moment.unix(currentCityDate).format('MM/DD/YYYY');
+    //         var currentDateEl = $('<span>');
+    //         currentDateEl.text(` (${currentCityDate}) `);
+    //         cityNameEl.append(currentDateEl);
+
+    //         var currentCityWeatherIcon = data.current.weather[0].icon;
+    //         var currentWeatherIconEl = $('<img>');
+    //         currentWeatherIconEl.attr("src", "http://openweathermap.org/img/wn/" + currentCityWeatherIcon + ".png");
+    //         cityNameEl.append(currentWeatherIconEl);
+
+    //         // get temperature data
+    //         var currentCityTemp = data.current.temp;
+    //         var currentTempEl = $('<p>')
+    //         currentTempEl.text(`Temp: ${currentCityTemp}`)
+    //         currentConditionsEl.append(currentTempEl);
+
+    //         // get wind
+    //         var currentCityWind = data.current.wind_speed;
+    //         var currentWindEl = $('<p>')
+    //         currentWindEl.text(`Wind: ${currentCityWind}`)
+    //         currentConditionsEl.append(currentWindEl);
+
+    //         // get humidity
+    //         var currentCityHumidity = data.current.humidity;
+    //         var currentHumidityEl = $('<p>')
+    //         currentHumidityEl.text(`Humidity: ${currentCityHumidity}`)
+    //         currentConditionsEl.append(currentHumidityEl);
+
+    //         // get UV index and set background color based on index
+    //         var currentCityUv = data.current.uvi;
+    //         var currentUvEl = $('<p>');
+    //         var currentUvSpanEl = $('<span>');
+    //         currentUvEl.append(currentUvSpanEl);
+
+    //         currentUvSpanEl.text(`UV Index: ${currentCityUv}`)
+
+    //         if (currentCityUv < 3 ) {
+    //             currentUvSpanEl.css({'background-color':'green', 'color':'white'});
+    //         } else if (currentCityUv < 6 ) {
+    //             currentUvSpanEl.css({'background-color':'yellow', 'color':'black'});
+    //         } else if (currentCityUv < 8 ) {
+    //             currentUvSpanEl.css({'background-color':'orange', 'color':'white'});
+    //         } else if (currentCityUv < 11 ) {
+    //             currentUvSpanEl.css({'background-color':'red', 'color':'white'});
+    //         } else {
+    //             currentUvSpanEl.css({'Background-color':'violet', 'color':'white'});
+    //         }
+
+    //         currentConditionsEl.append(currentUvEl);
+    //         currentConditionsEl.classList.remove("hide");
+
+    //         // get 5 day forecast
+    //         var fiveDayForecastEl = $('.forecast-container');
+
+    //         for (var i = 1; i <=5; i++) {
+    //             var date;
+    //             var temp;
+    //             var icon;
+    //             var wind;
+    //             var humidity;
+    //             var uvIndex;
+
+    //             date = data.daily[i].dt;
+    //             date = moment.unix(date).format('MM/DD/YYYY');
+
+    //             temp = data.daily[i].temp.day;
+    //             icon = data.daily[i].weather[0].icon;
+    //             wind = data.daily[i].wind_speed;
+    //             humidity = data.daily[i].humidity;
+    //             uvIndex = data.daily[i].uvi;
+
+    //             // creating cards for each day of forecast
+    //             var card = document.createElement('section');
+    //             card.classList.add('card', 'col-2', 'm-1', 'bg-primary', 'text-white');
+
+    //             var cardBody = document.createElement('section');
+    //             cardBody.classList.add('card-body')
+    //             cardBody.innerHTML = `<h4>${date}</h4>
+    //                 <img src= "https://openweathermap.org/img/wn/${icon}.png"></><br>
+    //                 ${temp}<br>
+    //                 ${wind}<br>
+    //                 ${humidity}<br>
+    //                 ${uvi}`
+
+    //             card.appendChild(cardBody)
+    //             fiveDayForecastEl.append(card);
+    //             fiveDayForecastEl.classList.remove('hide');
+    //         }
+    //     })
+    // return;
+}
+
 // function to get city coordinates to pass to one call ApI
 function getCoordinates() {
     var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${APIkey}`;
